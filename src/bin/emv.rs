80dc3d2b480334b6b1cfd7d1642d1cadb66e0df1
Avery Hankins <164415993+avery-hankins@users.noreply.github.com>
2025-12-23 17:36:16 -0800

    emv

--- src/bin/emv.rs
+++ src/bin/emv.rs
@@ -1,3 +1,27 @@
+use std::env;
+use eutils::*;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 1 {
+        print_help();
+        return;
+    }
+
+    let config_path = get_config_path();
+    let preferences = parse_config(&config_path);
+
+    let source_files = &args[1..args.len()-1];
+    let destination = &args[args.len() - 1];
+
+    execute_on(source_files, destination, true, preferences);
 }
+
+fn print_help() {
+    // usage: mv [-f | -i | -n] [-hv] source target
+    //        mv [-f | -i | -n] [-v] source ... directory
+    
+    println!("usage: emv source ... target");
+}
+
